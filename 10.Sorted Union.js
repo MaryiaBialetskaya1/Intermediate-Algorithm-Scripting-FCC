ffa2690022d1ba89ec38395a54f17ac6284d1e10
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-14 13:06:17 -0500

    add solution

--- 10.Sorted Union.js
+++ 10.Sorted Union.js
@@ -5,3 +5,27 @@
 // The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
 
 // Check the assertion tests for examples.
+
+function uniteUnique(arr) {
+    // Creates an empty array to store our final result.
+    const finalArray = [];
+
+    // Loop through the arguments object to truly make the program work with two or more arrays
+    // instead of 3.
+    for (let i = 0; i < arguments.length; i++) {
+      const arrayArguments = arguments[i];
+
+      // Loops through the array at hand
+      for (let j = 0; j < arrayArguments.length; j++) {
+        let indexValue = arrayArguments[j];
+
+        // Checks if the value is already on the final array.
+        if (finalArray.indexOf(indexValue) < 0) {
+          finalArray.push(indexValue);
+        }
+      }
+    }
+
+    return finalArray;
+  }
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
