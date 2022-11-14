2061de73a3c9bb9af2d31ef6ffe128226f1aea8e
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-14 13:21:22 -0500

    add code explanation

--- 12.Sum All Odd Fibonacci Numbers.js
+++ 12.Sum All Odd Fibonacci Numbers.js
@@ -19,3 +19,11 @@ function sumFibs(num) {
   }
   sumFibs(4);
   console.log(sumFibs(4))
+
+
+//   Code Explanation
+//   Create a variable to keep record of the current and previous numbers along with the result that will be returned.
+//   Use a while loop to make sure we do not go over the number given as parameter.
+//   We use the modulo operand to check if the current number is odd or even. If it is odd, add it to the result.
+//   Complete the Fibonacci circle by rotating getting the next number and swapping values after.
+//   Return the result.
