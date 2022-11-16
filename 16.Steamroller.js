dd65ab90171c9aefa65ca139952f0d466e68b03e
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-16 18:32:41 -0500

    add explanation

--- 16.Steamroller.js
+++ 16.Steamroller.js
@@ -29,3 +29,10 @@ function steamrollArray_2(arr) {
 
 steamrollArray_2([1, [2], [3, [[4]]]]);
 console.log(steamrollArray_2([1, [2], [3, [[4]]]]));
+
+//explanation:
+// Code Explanation
+// Use spread operator to concatenate each element of arr with an empty array
+// Use Array.some() method to find out if the new array contains an array still
+// If it does, use recursion to call steamrollArray again, passing in the new array to repeat the process on the arrays that were deeply nested
+// If it does not, return the flattened array
