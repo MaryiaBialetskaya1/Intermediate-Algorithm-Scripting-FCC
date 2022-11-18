9348bebfd55259dd446325e937989b8a430fe45d
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-17 20:44:04 -0500

    add solution

---
+++ 21.Map the Debris.js
@@ -0,0 +1,28 @@
+//TASK:
+
+/**
+ *Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+ */
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const newArr = [];
+
+  //Looping through each key in arr object
+  for (let elem in arr) {
+    //Rounding off the orbital period value
+    const orbitalPer = Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(arr[elem].avgAlt + earthRadius, 3) / GM)
+    );
+    //Adding new object with orbitalPeriod property
+    newArr.push({ name: arr[elem].name, orbitalPeriod: orbitalPer });
+  }
+
+  return newArr;
+}
+
+// test here
+orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
